9abd9ce6170312798cd18ab73f79343c49302257
jiduslove <oondayr@gamil.com>
2023-04-01 23:36:03 +0900

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="App">Hello, React!</div>;
+  const result = 20;
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold mb-4">계산기</h1>
+      <div className="text-3xl font-black mb-4">{result}</div>
+      <div className="mb-4"></div>
+    </div>
+  );
 }
 
 export default App;
